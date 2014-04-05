d0980235a5d41a10af1ae42cc0561eb186c3248b
Anderson Mesquita <andersonvom@gmail.com>
2014-04-05 16:52:06 -0400

    Normalize transactions
    This normalizes a specific transaction imported from a TAB file into the
    correct models/tables, preventing data duplication.  If any errors occur
    during the process, the information is stored for future debugging.

--- importer/app/models/import_item.rb
+++ importer/app/models/import_item.rb
@@ -1,3 +1,6 @@
+require 'csv'
+
+
 class ImportItem < ActiveRecord::Base
   belongs_to :import
 
@@ -5,21 +8,53 @@ class ImportItem < ActiveRecord::Base
                 :purchase_count, :merchant_address, :merchant_name
 
   def self.import(attrs)
-    item = self.new(attrs)
-    if item.imported?
-      logger.info("\tSkipping line #{item.line_number}. Already imported.")
+    import_item = self.new(attrs)
+    if import_item.imported?
+      logger.info("\tSkipping line #{import_item.line_number}. Already imported.")
     else
-      item.normalize!
+      logger.debug("\tNormalizing line #{import_item.line_number}")
+      import_item.normalize!
     end
   end
 
   def imported?
-    self.class.where(import: import, line_number: line_number).size > 0
+    obj = self.class.find_by(import: import, line_number: line_number)
+    obj and obj.data == nil
   end
 
   def normalize!
-    # TODO: save all models
-    logger.debug("\tNormalizing line #{line_number}")
+    begin
+      import_info!
+    rescue => exc
+      logger.error("\tFailed to normalize line #{line_number}: #{exc.message}")
+      save_data_for_logs!
+    end
+  end
+
+  private
+
+  def import_info!
+    self.transaction do
+      purchaser = Purchaser.find_or_create_by(name: purchaser_name)
+      merchant = Merchant.find_or_create_by(name: merchant_name)
+      location = merchant.locations.find_or_create_by(address: merchant_address)
+      item = merchant.items.find_or_create_by(description: item_description)
+      inventory_item = location.create_or_update_inventory_item(item, item_price)
+      Purchase.create(import: import,
+                      purchaser: purchaser,
+                      inventory_item: inventory_item,
+                      count: purchase_count)
+      save
+    end
+  end
+
+  def save_data_for_logs!
+    self.data = all_attrs.to_csv
     save
+  end
+
+  def all_attrs
+    [purchaser_name, item_description, item_price,
+     purchase_count, merchant_address, merchant_name]
   end
 end
--- importer/spec/models/import_item_spec.rb
+++ importer/spec/models/import_item_spec.rb
@@ -3,33 +3,113 @@ require 'spec_helper'
 describe ImportItem do
 
   before :each do
-    @item = ImportItem.new
+    @item = ImportItem.new(
+      purchaser_name: 'purchaser', item_description: 'desc',
+      item_price: '10.50', purchase_count: '5',
+      merchant_address: 'address', merchant_name: 'merchant'
+    )
   end
 
-  it "should know where a given item has already been imported" do
-    ImportItem.should_receive(:where).and_return([])
-    @item.imported?.should be_false
+  describe "#imported?" do
+    it "should return false if item not in DB" do
+      ImportItem.should_receive(:find_by).and_return(nil)
+      @item.imported?.should be_false
+    end
 
-    ImportItem.should_receive(:where).and_return([1, 2, 3])
-    @item.imported?.should be_true
+    it "should return false if item has no error data" do
+      @item.data = "foo error"
+      ImportItem.should_receive(:find_by).and_return(@item)
+      @item.imported?.should be_false
+    end
+
+    it "should return true if item in DB and no error data" do
+      ImportItem.should_receive(:find_by).and_return(@item)
+      @item.imported?.should be_true
+    end
   end
 
-  context "importing item" do
+  describe "#self.import" do
+    before :each do
+      ImportItem.should_receive(:new).and_return(@item)
+    end
+
+    it "should skip items already normalized and log it" do
+      @item.should_receive(:imported?).and_return(true)
+      @item.should_not_receive(:normalize!)
+      ImportItem.import({foo: 'bar'})
+    end
+
     it "should normalize items not yet imported" do
       @item.should_receive(:imported?).and_return(false)
       @item.should_receive(:normalize!)
-      ImportItem.should_receive(:new).and_return(@item)
-
-      ImportItem.import({foo: 'bar'})
-    end
-
-    it "should normalize items not yet imported" do
-      @item.should_receive(:imported?).and_return(true)
-      @item.should_not_receive(:normalize!)
-      ImportItem.should_receive(:new).and_return(@item)
-
       ImportItem.import({foo: 'bar'})
     end
   end
 
+  describe "#normalize!" do
+    context "success" do
+      it "should import item information" do
+        @item.should_receive(:import_info!)
+        @item.normalize!
+      end
+
+      it "should import item information in a transaction" do
+        @item.should_receive(:transaction)
+        @item.normalize!
+      end
+
+      it "should import item information in a transaction" do
+        @item.import = Import.new
+        @item.should_receive(:save)
+        purchaser = Purchaser.new
+        merchant = Merchant.new
+        location = merchant.locations.new
+        item = merchant.items.new
+        inventory_item = location.inventory_items.new item: item,
+                                                      price_in_cents: 1050
+
+        Purchaser.should_receive(:find_or_create_by)
+                 .with(name: 'purchaser')
+                 .and_return(purchaser)
+        Merchant.should_receive(:find_or_create_by)
+                .with(name: 'merchant')
+                .and_return(merchant)
+        merchant.locations.should_receive(:find_or_create_by)
+                          .with(address: 'address')
+                          .and_return(location)
+        merchant.items.should_receive(:find_or_create_by)
+                      .with(description: 'desc')
+                      .and_return(item)
+        location.should_receive(:create_or_update_inventory_item)
+                .with(item, '10.50')
+                .and_return(inventory_item)
+
+        Purchase.should_receive(:create).with(
+          import: @item.import,
+          purchaser: purchaser,
+          inventory_item: inventory_item,
+          count: '5',
+        )
+
+        @item.normalize!
+      end
+    end
+
+    context "error" do
+      before :each do
+        @item.should_receive(:import_info!).and_raise
+      end
+
+      it "should save error data in error occurs" do
+        @item.should_receive(:save_data_for_logs!)
+        @item.normalize!
+      end
+
+      it "should save all attributes in CSV" do
+        @item.should_receive(:save)
+        @item.normalize!
+        @item.data.should == "purchaser,desc,10.50,5,address,merchant\n"
+      end
+    end
+  end
 end
