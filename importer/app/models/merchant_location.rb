4b1f4621b2e3b0a2a2306520948451d7c0ca52ba
Anderson Mesquita <andersonvom@gmail.com>
2014-04-05 19:33:26 -0400

    Create or update inventory item
    Because inventory items are unique per item and location but also have
    to have prices, this looks the item up first, initializing it if
    necessary, updates the price on it and saves it to the DB.

--- importer/app/models/merchant_location.rb
+++ importer/app/models/merchant_location.rb
@@ -2,4 +2,11 @@ class MerchantLocation < ActiveRecord::Base
   belongs_to :merchant
   has_many :inventory_items
   has_many :items, through: :inventory_items
+
+  def create_or_update_inventory_item(item, price)
+    inventory_item = inventory_items.find_or_initialize_by(item: item)
+    inventory_item.price = price
+    inventory_item.save
+    inventory_item
+  end
 end
--- importer/spec/models/merchant_location_spec.rb
+++ importer/spec/models/merchant_location_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe MerchantLocation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @location = MerchantLocation.new
+    @item = InventoryItem.new
+  end
+
+  it "should create inventory items if they don't exist" do
+    all_items = @location.inventory_items
+    all_items.should_receive(:find_or_initialize_by).and_return(@item)
+    @item.should_receive(:save)
+    @location.create_or_update_inventory_item(@item, 10)
+    @item.price.should == 10
+  end
+
+  it "should update inventory item price if they exist" do
+    @item.price = 10
+    all_items = @location.inventory_items
+    all_items.should_receive(:find_or_initialize_by).and_return(@item)
+    @item.should_receive(:save)
+    @location.create_or_update_inventory_item(@item, 20)
+    @item.price.should == 20
+  end
 end
